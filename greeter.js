1cc13eae8b0b42d6301dd5097a7bd8cf6325ab0c
ciara nelson <ciara.l.nelson@live.com>
2017-07-18 19:28:05 -0400

    javascript time

---
+++ greeter.js
@@ -0,0 +1,19 @@
+
+    prompt('What is your name?');
+    alert('Hello World!');
+
+
+      var name = prompt('Ciara')
+      alert('Hello' + name);
+      var myCountry = prompt('New Zealand');
+      var FreedomCheck = function(){
+        if( myCountry === 'USA' ) alert('Sweet Land of Liberty');
+        else alert('Sounds like you need some DEMOCRACY');
+      };
+      var occupation = prompt ('web developer');
+      var JobSearch = prompt ('What is your dream job?');
+      var Salary = function(){
+      if (Salary === 'Classified') alert('That means top secret buddy!');
+      else alert('Sounds like you\'re being nosy');
+      };
+      FreedomCheck();
